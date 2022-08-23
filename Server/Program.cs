3b8c16f8ce15520a6eba1fbfacf6135ec838fda0
Bassel Amgad <bamgad7@gmail.com>
2022-08-23 13:27:38 +0200

    Added anti-forgery token.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.AspNetCore.Antiforgery;
+
 var builder = WebApplication.CreateBuilder(args);
 
 using IHost host = Host.CreateDefaultBuilder(args).Build();
@@ -25,80 +27,91 @@ app.UseSwaggerUI(options =>
     options.RoutePrefix = string.Empty;
 });
 
-app.MapGet("recipes", async () =>
+app.MapGet("recipes", async (HttpContext context, IAntiforgery antiForgery) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     var recipes = await data.GetRecipesAsync();
     return Results.Ok(recipes);
 });
 
-app.MapGet("/recipes/{id}", async (Guid id) =>
+app.MapGet("/recipes/{id}", async (HttpContext context, IAntiforgery antiForgery, Guid id) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     Recipe recipe = await data.GetRecipeAsync(id);
     return Results.Ok(recipe);
 });
 
-app.MapPost("/recipes", async (Recipe recipe) =>
+app.MapPost("/recipes", async (HttpContext context, IAntiforgery antiForgery, Recipe recipe) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     recipe.Id = Guid.NewGuid();
     await data.AddRecipeAsync(recipe);
     return Results.Created($"/recipes/{recipe.Id}",recipe);
 });
     
-app.MapPut("/recipes/{id}", async (Guid id, Recipe newRecipe) =>
+app.MapPut("/recipes/{id}", async (HttpContext context, IAntiforgery antiForgery, Guid id, Recipe newRecipe) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     var updatedRecipe =await data.EditRecipeAsync(id, newRecipe);
     return Results.Ok(updatedRecipe);
 });
 
-app.MapDelete("/recipes/{id}", async (Guid id) =>
+app.MapDelete("/recipes/{id}", async (HttpContext context, IAntiforgery antiForgery, Guid id) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     await data.RemoveRecipeAsync(id);
     return Results.Ok();
 });
 
-app.MapGet("/categories", async () =>
+app.MapGet("/categories", async (HttpContext context, IAntiforgery antiForgery) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     var categories = await data.GetAllCategoriesAsync();
     return Results.Ok(categories);
 
 });
 
-app.MapPost("/categories", async (string category) =>
+app.MapPost("/categories", async (HttpContext context, IAntiforgery antiForgery, string category) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     await data.AddCategoryAsync(category);
     return Results.Created($"/categories/{category}",category);
 });
 
-app.MapPut("/categories", async (string category, string newCategory) =>
+app.MapPut("/categories", async (HttpContext context, IAntiforgery antiForgery, string category, string newCategory) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     await data.EditCategoryAsync(category, newCategory);
     return Results.Ok($"Category ({category}) updated to ({newCategory})");
 });
 
-app.MapDelete("/categories", async (string category) =>
+app.MapDelete("/categories", async (HttpContext context, IAntiforgery antiForgery, string category) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     await data.RemoveCategoryAsync(category);
     return Results.Ok();
 });
 
-app.MapPost("recipes/category", async (Guid id ,string category) =>
+app.MapPost("recipes/category", async (HttpContext context, IAntiforgery antiForgery, Guid id ,string category) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     await data.AddCategoryToRecipeAsync(id,category);
     return Results.Created($"recipes/category/{category}",category);
 });
 
-app.MapDelete("recipes/category", async (Guid id, string category) =>
+app.MapDelete("recipes/category", async (HttpContext context, IAntiforgery antiForgery, Guid id, string category) =>
 {
+    await antiForgery.ValidateRequestAsync(context);
     Data data = new(app.Logger);
     await data.RemoveCategoryFromRecipeAsync(id,category);
     return Results.Ok();
